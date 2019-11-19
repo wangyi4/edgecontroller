0c0a93ee01f59711dfdcf3af8b54eeca74661d51
Yi Wang <yi.c.wang@intel.com>
2019-11-19 09:55:25 +0800

    add "edgecontroller" prefix to image names
    Signed-off-by: Yi Wang <yi.c.wang@intel.com>

--- stx-build/upload_images.sh
+++ stx-build/upload_images.sh
@@ -6,11 +6,11 @@ REPO=edgecontroller-vm.sh.intel.com:5000
 VER=19.06
 
 # tag images
-docker tag cce:latest $REPO/cce:$VER
-docker tag ui:latest $REPO/ui:$VER
-docker tag cups:latest $REPO/cups:$VER
+docker tag cce:latest $REPO/edgecontroller-cce:$VER
+docker tag ui:latest $REPO/edgecontroller-ui:$VER
+docker tag cups:latest $REPO/edgecontroller-cups:$VER
 
 # upload images
-docker push $REPO/cce:$VER
-docker push $REPO/ui:$VER
-docker push $REPO/cups:$VER
+docker push $REPO/edgecontroller-cce:$VER
+docker push $REPO/edgecontroller-ui:$VER
+docker push $REPO/edgecontroller-cups:$VER
